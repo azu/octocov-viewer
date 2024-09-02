393dd308c30cf28415278446a497321b62db7381
azu <azuciao@gmail.com>
2024-09-02 23:47:15 +0900

    Update .github/workflows/ci-go.yaml+4

---
+++ go/hello.go
@@ -0,0 +1,17 @@
+package hello
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return name, errors.New("empty name")
+	}
+	// Create a message using a random format.
+	message := fmt.Sprintf("Hello %s", name)
+	return message, nil
+}
